0817fdc6cc26c71b66435d8fb7ceb5b8eb4f03da
qinzhonghua <qinzhonghua@huaqin.com>
2014-07-18 15:39:15 +0800

    update code

--- src/com/example/samplecirclesdefault/SampleCirclesDefault.java
+++ src/com/example/samplecirclesdefault/SampleCirclesDefault.java
@@ -1,9 +1,12 @@
 package com.example.samplecirclesdefault;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-
+import android.view.LayoutInflater;
+import android.view.View;
 import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.PageIndicator;
 
@@ -16,9 +19,15 @@ public class SampleCirclesDefault extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_circles);
-
-        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
-
+        LayoutInflater mLi = LayoutInflater.from(this);
+        View view1 = mLi.inflate(R.layout.image1_layout, null);
+        View view2 = mLi.inflate(R.layout.img_layout, null);
+        View view3 = mLi.inflate(R.layout.image2_layout, null);
+        final ArrayList<View> views = new ArrayList<View>();
+        views.add(view1);
+        views.add(view2); 
+        views.add(view3);
+        mAdapter = new TestFragmentAdapter(views);
         mPager = (ViewPager)findViewById(R.id.pager);
         mPager.setAdapter(mAdapter);
 
--- src/com/example/samplecirclesdefault/TestFragmentAdapter.java
+++ src/com/example/samplecirclesdefault/TestFragmentAdapter.java
@@ -1,39 +1,41 @@
 package com.example.samplecirclesdefault;
 
 
+import java.util.ArrayList;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
-class TestFragmentAdapter extends FragmentPagerAdapter  {
-    protected static final String[] CONTENT = new String[] { "This", "Is", "A", "Test", };
-
-    private int mCount = CONTENT.length;
-
-    public TestFragmentAdapter(FragmentManager fm) {
-        super(fm);
+class TestFragmentAdapter extends PagerAdapter  {
+	private ArrayList<View> views;
+	private ArrayList<String> titles;
+    public TestFragmentAdapter(ArrayList<View> views) {
+    	this.views = views;
+    	
     }
 
-    @Override
-    public Fragment getItem(int position) {
-        return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
-    }
+	@Override
+	public Object instantiateItem(View container, int position) {
+		((ViewPager) container).addView(views.get(position));
+		return views.get(position);
+	}
 
-    @Override
-    public int getCount() {
-        return mCount;
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-      return TestFragmentAdapter.CONTENT[position % CONTENT.length];
-    }
-
-
-    public void setCount(int count) {
-        if (count > 0 && count <= 10) {
-            mCount = count;
-            notifyDataSetChanged();
-        }
-    }
+	@Override
+	public boolean isViewFromObject(View arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return arg0 == arg1;
+	}
+	//一定要销毁！！！否则会出错
+	public void destroyItem(View container, int position, Object object) {
+		((ViewPager) container).removeView(views.get(position));
+	}
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return this.views.size();
+	}
 }
